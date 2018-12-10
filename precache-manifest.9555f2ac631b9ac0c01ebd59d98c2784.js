60c2e78c814d45afaa8e51ceb1b4d061b0f73fdf
Cameron  Partee <cameronpartee@Cammyp-newBook-Pro.fios-router.home>
2018-12-10 18:20:32 -0500

    Updates

---
+++ precache-manifest.9555f2ac631b9ac0c01ebd59d98c2784.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/upload/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "cd57f327f3ae7a420cbd",
+    "url": "/upload/static/js/runtime~main.cd57f327.js"
+  },
+  {
+    "revision": "314ec8a96a25ed4d0659",
+    "url": "/upload/static/js/main.314ec8a9.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/upload/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "314ec8a96a25ed4d0659",
+    "url": "/upload/static/css/main.b4afbf30.chunk.css"
+  },
+  {
+    "revision": "e533d9123b94d6273a83982b9c4452d6",
+    "url": "/upload/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.184047f0f276149d5436b76429d9e205.js"
+  "/upload/precache-manifest.9555f2ac631b9ac0c01ebd59d98c2784.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/upload/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
